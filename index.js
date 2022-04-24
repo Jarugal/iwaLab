c704ab2a0a20d9e6ba74ca920fe3c1c20e6d5765
Jarugal <jargal620@gmail.com>
2022-04-24 22:56:46 +0000

    march11 changes

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const http = require("http"),
-      logger = require("morgan"),
-      express = require("express"),
-      bodyParser = require("body-parser");
-    
- let app = express();
- let port = 8000;
- 
-app.use(bodyParser.json());
+const   http = require("http"),
+        logger = require("morgan"),
+        express = require("express"),
+        bodyParser = require("body-parser");
 
+let app = express();
+let port = 8000;
+
+app.use(require('./routes'));
+app.use(bodyParser.json());
 app.use(logger("tiny"));
-app.get("/", (res, req)=>{
-    res.json({message: "Hello world"})
-});
 
 app.listen(port, function(err){
     console.log("Listening on port: " + port)
- });
+
+
+});
